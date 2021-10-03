de29d439743e5faa72160b60ab8507209b35f5a5
Moshe Rot <mrot@proofpoint.com>
2021-10-03 13:53:41 +0300

    faust ok

--- project1/consumers/faust_stream.py
+++ project1/consumers/faust_stream.py
@@ -1,6 +1,5 @@
 """Defines trends calculations for stations"""
 import logging
-
 import faust
 
 
@@ -8,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 # Faust will ingest records from Kafka in this format
-class Station(faust.Record):
+class Station(faust.Record, validation=True):
     stop_id: int
     direction_id: str
     stop_name: str
@@ -22,36 +21,45 @@ class Station(faust.Record):
 
 
 # Faust will produce records to Kafka in this format
-class TransformedStation(faust.Record):
+class TransformedStation(faust.Record, validation=True):
     station_id: int
     station_name: str
     order: int
     line: str
 
 
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
-app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+app = faust.App("stations-stream-7", broker="kafka://localhost:9092", store="memory://")
+topic = app.topic("org.stations", value_type=Station)
+out_topic = app.topic("org.stations.table.updates", partitions=1)
+
+table = app.Table(
+   'org.stations.table.v1',
+   default=TransformedStation,
+   partitions=1,
+   changelog_topic=out_topic,
+)
 
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+@app.agent(topic)
+async def proc_stations(raw_stations: faust.Stream):
+    async for raw_station in raw_stations:
+        if raw_station.green is True:
+            line = 'green'
+        elif raw_station.blue is True:
+            line = 'blue'
+        elif raw_station.red is True:
+            line = 'red'
+        else:
+            line = 'N/A'
+
+        table[raw_station.station_id] = TransformedStation(
+            station_id = raw_station.station_id,
+            station_name = raw_station.station_name,
+            order = raw_station.order,
+            line = line
+        )
+
+        print(raw_station.station_id)
 
 
 if __name__ == "__main__":
